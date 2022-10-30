b182cda8e5bd6ef9453a107cc558d12e19176f72
han <pure33883833@gmail.com>
2022-10-31 01:54:57 +0900

    2

---
+++ rere_javascript/1/main.js
@@ -0,0 +1,55 @@
+// const animal = ["a", "b", "c"]
+
+// animal.pop()
+
+// console.log(animal)
+
+// animal.unshift("hippo")
+// const food = animal === "lion" ? "meet" : 
+// console.log(animal)
+
+// swich문
+// const animal = "lion"
+
+// swich(animal){
+//     case "lion":
+//         console.log()
+// }
+
+// function add(a = 1, b = 2){
+//     return a+b
+// }
+
+// console.log(add)
+
+// 애로우 함수
+// const add = (a, b) => {
+//   a + b
+// }
+
+// 매개변수 하나면 가로 생략 가능 아래는 a 하나인 경우
+// const add = a => a + b
+
+
+// 
+
+// let str = 'ans'
+// let str2 = 'wer'
+
+// let n = str.charAt(2)
+
+// alert(n)
+// let timer = setTimeout(function(){alert('힘들죠')}, 3000)
+// // clearTimeout(timer2)
+// // let timer2 = setInterval(function(){alert('힘들죠')}, 3000);
+// let imageFile = "<img src=`./components/images/Frame' + i + '.png`>";
+// console.log(imageFile.length, "이미지 개수")
+
+// for(let i=0;i<imageFile.length){
+
+// }
+document.write("<img src = \"./images/Frame1.png\">")
+
+
+
+
